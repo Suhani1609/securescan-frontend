edda8f68362671e4c8dd43d341ea0a77b4460a5e
Suhani1609 <suhanimpatel2005@gmail.com>
2025-12-24 00:48:50 +0530

    SecureScan frontend ready

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  // baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://securescan-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
